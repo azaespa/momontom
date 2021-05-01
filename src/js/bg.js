07f78ac29f0c789ae76909adbaa45ab1c8a5878a
letsdothispej <pauljamesazanes@gmail.com>
2021-05-01 12:32:57 +0800

    random bg imgs

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,13 +1,15 @@
 const body = document.querySelector('body'),
     background = body.querySelector('.background');
 
-const NUMBER_OF_IMG = 6;
+const NUMBER_OF_IMG = 4;
+const BG_IMGS = [
+    "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80",
+    "https://images.unsplash.com/photo-1616927366799-d4cab1b7e545?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1400&q=80",
+    "https://images.unsplash.com/photo-1618590837297-dfa199c850ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1402&q=80",
+    "https://images.unsplash.com/photo-1618711103795-a20af977b6e9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1527&q=80"];
 
 function paintBg(imgNumber){
-    const bg = new Image();
-    bg.src = `images/${imgNumber + 1}.jpg`;
-    bg.classList.add("bgImage");
-    body.prepend(bg);
+    body.style.backgroundImage = `url(${BG_IMGS[imgNumber]})`;
 }
 
 function genRandom(){
@@ -16,8 +18,7 @@ function genRandom(){
 }
 
 function init(){
-    //paintBg(genRandom());
-    body.style.backgroundImage = "url(https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1500&q=80)";
+    paintBg(genRandom());
 }
 
 init();
