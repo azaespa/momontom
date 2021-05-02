6a0b70d8c8893dc2216eb89ca3174bf0b11afac2
letsdothispej <pauljamesazanes@gmail.com>
2021-05-02 22:52:39 +0800

    added weather

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,8 +1,17 @@
+const weatherText = document.querySelector(".weather");
+
 const API_KEY = '61af2c7d44b3892f85e7d7b9dffa159d';
 const COORDS = 'coords';
 
 function getWeather(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    ).then(function (response){
+        return response.json();
+    }).then(function (json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weatherText.innerText = `${temperature} @ ${place}`;
+    })
 }
 
 function saveCoords(coordsObj){
